4e8b5289edd250562a24dcf192ef69f963fe1b36
davemuir <davidmuirdesign@gmail.com>
2012-07-05 00:26:46 -0400

    now

---
+++ wp-content/themes/imbalance/footer.php
@@ -0,0 +1,46 @@
+       
+    </div><!--end of outer-->
+     <div id="footer">
+     	<div id="innerFooter">
+        	<!--<div id="copyright">&copy; <?php the_time(__('Y')) ?> <?php bloginfo('name'); ?>. All Rights Reserved.</div>-->
+            <!--<div id="credits">Powered by Wordpress. Designed by <a href="http://wpshower.com/">WPSHOWER</a></div>-->
+            <table id="tableFooter">
+            	<tr>
+            		<td>
+            			<div class="celly">
+							Contact:
+								<ul>	
+									<li>David Muir</li>
+									<li>1 416 858 0128</li>
+									<li>davidmuirdesign@gmail.com</li>
+									<li>item 1</li>
+									
+								</ul>
+						</div>
+					</td>
+            		<td>
+            			<div class="celly">
+							Services:
+								<ul>	
+									<li>Static/Dynamic/Responsive/CMS Websites</li>
+									<li>Social Media Solutions and Integration</li>
+									<li>Mobile Solutions</li>
+									<li>Web Development Quotes</li>
+									
+								</ul>
+						</div>
+            		</td>
+            		<td>
+            			<div class="celly">
+							<img src="/wp-content/uploads/2012/07/faceThumb.png">
+							<img src="/wp-content/uploads/2012/07/gmailThumb.png">
+							<img src="/wp-content/uploads/2012/07/twitterThumb.png">
+						</div>
+            		</td>
+            	</tr>
+            </table>
+        </div>
+        </div>
+<?php wp_footer(); ?>
+</body>
+</html>
